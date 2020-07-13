0b2909e75228112c28c7f7957554c40a12938a8a
Maurício Spinardi <mauricio.spinardi@setis.com.br>
2020-07-13 15:44:16 -0300

    Atualiza a janela principal (para exibir imagens)

--- src/main.cpp
+++ src/main.cpp
@@ -11,73 +11,18 @@
 
 #include <gtk/gtkx.h>
 
-/**
- * @brief 
- * 
- * @param application 
- * @param user_data 
- */
+/*************************/
+/* Protótipos de funções */
+/*************************/
+
 static void
-activate(GtkApplication *application, gpointer user_data)
-{
-    GtkWidget *window;
-    GtkWidget *label;
+activate(GtkApplication *application, gpointer user_data);
 
-    (void) user_data;
+static gboolean
+button_press_callback(GtkWidget *event_box, GdkEventButton *event, gpointer data);
 
-    window = gtk_application_window_new(application);
-
-    label = gtk_label_new("Hello GNOME!");
-
-    gtk_container_add(GTK_CONTAINER(window), label);
-
-    gtk_window_set_title(GTK_WINDOW(window), "Welcome to GNOME");
-
-    gtk_window_set_default_size(GTK_WINDOW(window), 320, 480);
-
-    gtk_widget_show_all(window);
-}
-
-/**
- * @brief 
- * 
- * @param event_box 
- * @param event 
- * @param data 
- * @return gboolean 
- */
-// static gboolean
-// button_press_callback(GtkWidget *event_box, GdkEventButton *event, gpointer data)
-// {
-//     (void) event_box;
-//     (void) data;
-
-//     g_print("Event box clicked at coordinates %f,%f\n", event->x, event->y);
-
-//     return TRUE;
-// }
-
-/**
- * @brief 
- * 
- * @return GtkWidget* 
- */
-// static GtkWidget*
-// create_image(void)
-// {
-//     GtkWidget *event_box;
-//     GtkWidget *image;
-
-//     image = gtk_image_new_from_file("myfile.png");
-
-//     event_box = gtk_event_box_new();
-
-//     gtk_container_add (GTK_CONTAINER(event_box), image);
-
-//     g_signal_connect(G_OBJECT(event_box), "button_press_event", G_CALLBACK(button_press_callback), image);
-
-//     return image;
-// }
+static GtkWidget *
+create_image(void);
 
 /**************************/
 /* Funções externalizadas */
@@ -106,4 +51,76 @@ int main(int argc, char *argv[])
     g_object_unref(application);
 
     return status;
+}
+
+/******************/
+/* Funções locais */
+/******************/
+
+/**
+ * @brief 
+ * 
+ * @param application 
+ * @param user_data 
+ */
+static void
+activate(GtkApplication *application, gpointer user_data)
+{
+    GtkWidget *event_box;
+    GtkWidget *window;
+
+    (void)user_data;
+
+    window = gtk_application_window_new(application);
+
+    event_box = create_image();
+
+    gtk_container_add(GTK_CONTAINER(window), event_box);
+
+    gtk_window_set_title(GTK_WINDOW(window), "Itu Developers");
+
+    gtk_window_set_default_size(GTK_WINDOW(window), 320, 480);
+
+    gtk_widget_show_all(window);
+}
+
+/**
+ * @brief 
+ * 
+ * @param event_box 
+ * @param event 
+ * @param data 
+ * @return gboolean 
+ */
+static gboolean
+button_press_callback(GtkWidget *event_box, GdkEventButton *event, gpointer data)
+{
+    (void) event_box;
+    (void) data;
+
+    g_print("Event box clicked at coordinates %f,%f\n", event->x, event->y);
+
+    return TRUE;
+}
+
+/**
+ * @brief 
+ * 
+ * @return GtkWidget* 
+ */
+static GtkWidget *
+create_image(void)
+{
+    GtkWidget *event_box;
+    GtkWidget *image;
+
+    image = gtk_image_new_from_file("misc/200713153459.png");
+
+    event_box = gtk_event_box_new();
+
+    gtk_container_add(GTK_CONTAINER(event_box), image);
+
+    g_signal_connect(G_OBJECT(event_box), "button_press_event", G_CALLBACK(button_press_callback), image);
+
+    return event_box;
 }
