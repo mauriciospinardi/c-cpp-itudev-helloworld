2f31b2673b1f6d64efae77d9c8754ba840b2431a
Maurício Spinardi <mauricio.spinardi@gmail.com>
2020-07-13 22:18:35 -0300

    Atualiza aplicação para acomodar a primeira versão de API ItuDev

--- src/main.cpp
+++ src/main.cpp
@@ -11,9 +11,28 @@
 
 #include <gtk/gtkx.h>
 
+/*********************/
+/* Vairáveis globais */
+/*********************/
+
+static GtkApplication *application;
+
+static GtkWidget *window;
+
+static int status = -1; /* Não inicializada! */
+
 /*************************/
 /* Protótipos de funções */
 /*************************/
+
+static int
+itudev_end(void);
+
+static int
+itudev_helloworld(void);
+
+static int
+itudev_start(void);
 
 static void
 activate(GtkApplication *application, gpointer user_data);
@@ -37,20 +56,52 @@ create_image(void);
  * @return EXIT_xxx
  * 
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[])    /* Para pensar:
+                                     * 
+                                     * - Será main(...) o melhor ponto de
+                                     * entrada para a aplicação?
+                                     * 
+                                     * - itudev_main(...)
+                                     *
+                                     */
 {
-    GtkApplication *application;
-    int status;
+    int ret;
 
-    application = gtk_application_new(NULL, G_APPLICATION_FLAGS_NONE);
+    (void) argc;
+    (void) argv;
 
-    g_signal_connect(application, "activate", G_CALLBACK(activate), NULL);
+    ret = itudev_start();
 
-    status = g_application_run(G_APPLICATION(application), argc, argv);
+    g_print("\r\n%s::%s [%d]", __FUNCTION__, "itudev_start()", ret);
 
-    g_object_unref(application);
+    if (ret)
+    {
+        exit(EXIT_FAILURE);
+    }
 
-    return status;
+    ret = itudev_helloworld();      /* Para pensar:
+                                     *
+                                     * - itudev_file_load(...);
+                                     * - itudev_file_size(...);
+                                     * - itudev_image_draw(...);
+                                     * - itudev_image_flush(...);
+                                     * 
+                                     * - itudev_...
+                                     * 
+                                     */
+
+    g_print("\r\n%s::%s [%d]", __FUNCTION__, "itudev_helloworld()", ret);
+
+    if (ret)
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    ret = itudev_end();
+
+    g_print("\r\n%s::%s [%d]", __FUNCTION__, "itudev_end()", ret);
+
+    return (ret) ? EXIT_FAILURE : EXIT_SUCCESS;
 }
 
 /******************/
@@ -58,22 +109,38 @@ int main(int argc, char *argv[])
 /******************/
 
 /**
- * @brief Cria e ativa uma nova janela.
+ * @brief Libera recursos consumidos na execução de itudev_start().
  * 
- * @param application [in] Identificador da aplicação
- * @param user_data [in] Dados privados
+ * @return int -1 para erros; 0 para sucesso
  */
-static void
-activate(GtkApplication *application, gpointer user_data)
+static int
+itudev_end(void)
+{
+    if (application && !status)
+    {
+        g_object_unref(application);
+
+        application = NULL;
+    }
+
+    g_print("\r\n%s::%s [%lu]", __FUNCTION__, "application", (long unsigned int) application);
+
+    return 0;
+}
+
+/**
+ * @brief Exibe "Hello, World!" na janela principal da aplicação.
+ * 
+ * @return int -1 para erros; 0 para sucesso
+ */
+static int
+itudev_helloworld(void)
 {
     GtkWidget *event_box;
-    GtkWidget *window;
-
-    (void)user_data;
-
-    window = gtk_application_window_new(application);
 
     event_box = create_image();
+
+    g_print("\r\n%s::%s [%lu]", __FUNCTION__, "create_image()", (long unsigned int) event_box);
 
     gtk_container_add(GTK_CONTAINER(window), event_box);
 
@@ -82,6 +149,68 @@ activate(GtkApplication *application, gpointer user_data)
     gtk_window_set_default_size(GTK_WINDOW(window), 320, 480);
 
     gtk_widget_show_all(window);
+
+    return (!event_box) ? -1 : 0;
+}
+
+/**
+ * @brief Inicializa o motor de renderização (e eventos) da plataforma.
+ *
+ * @return int -1 para erros; 0 para sucesso
+ */
+static int
+itudev_start(void)
+{
+    if (status > 0)
+    {
+        return 0;
+    }
+
+    application = gtk_application_new(NULL, G_APPLICATION_FLAGS_NONE);
+
+    g_print("\r\n%s::%s [%lu]", __FUNCTION__, "gtk_application_new()", (long unsigned int) application);
+
+    status = (!application) ? -1 : 1;
+
+    if (status < 0)
+    {
+        return -1;
+    }
+
+    g_signal_connect(application, "activate", G_CALLBACK(activate), NULL);
+
+    status = g_application_run(G_APPLICATION(application), 0, NULL);
+
+    g_print("\r\n%s::%s [%d]", __FUNCTION__, "g_application_run()", status);
+
+    if (status)
+    {
+        return -1;
+    }
+
+    status = itudev_end();
+
+    g_print("\r\n%s::%s [%d]", __FUNCTION__, "itudev_end()", status);
+
+    exit(EXIT_SUCCESS);
+}
+
+/**
+ * @brief Cria nova janela e retoma o ponto de entrada da aplicação.
+ * 
+ * @param application [in] Identificador da aplicação
+ * @param user_data [in] Dados privados
+ */
+static void
+activate(GtkApplication *application, gpointer user_data)
+{
+    (void) user_data;
+
+    window = gtk_application_window_new(application);
+
+    g_print("\r\n%s::%s [%lu]", __FUNCTION__, "gtk_application_window_new()", (long unsigned int) window);
+
+    main(0, NULL);
 }
 
 /**
@@ -90,7 +219,7 @@ activate(GtkApplication *application, gpointer user_data)
  * @param event_box [in] GtkWidget
  * @param event [in] Evento
  * @param data [in] Dados privados
- * @return gboolean TRUE se o evento foi tratado ou FALSE, caso contrário.
+ * @return gboolean TRUE se o evento foi tratado; FALSE, caso contrário.
  */
 static gboolean
 button_press_callback(GtkWidget *event_box, GdkEventButton *event, gpointer data)
@@ -98,7 +227,7 @@ button_press_callback(GtkWidget *event_box, GdkEventButton *event, gpointer data
     (void) event_box;
     (void) data;
 
-    g_print("Event box clicked at coordinates %f,%f\n", event->x, event->y);
+    g_print("\r\n%s::%s %f,%f", __FUNCTION__, "Event box clicked at coordinates", event->x, event->y);
 
     return TRUE;
 }
@@ -116,7 +245,11 @@ create_image(void)
 
     image = gtk_image_new_from_file("misc/200713153459.png");
 
+    g_print("\r\n%s::%s [%lu]", __FUNCTION__, "gtk_image_new_from_file()", (long unsigned int) image);
+
     event_box = gtk_event_box_new();
+
+    g_print("\r\n%s::%s [%lu]", __FUNCTION__, "gtk_event_box_new()", (long unsigned int) event_box);
 
     gtk_container_add(GTK_CONTAINER(event_box), image);
 
